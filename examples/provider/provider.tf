5f05bcaf82da0b8da82895fa6e8fc9ae3be8f923
Ning Kang <nkang@agl.com.au>
2023-05-12 22:15:53 +1000

    commit 2

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_providers {
+    crud = {
+      source = "crud.com/crud/unicorns"
+    }
+  }
+}
 
 provider "crud" {
    endpoint = "https://crudcrud.com/api/b93947cfec7840c9aba7f57e2bae87e8/unicorns"
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
+	"terraform-crud-provider/internal/crud"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/ning-kang/terraform-crud-provider/internal/crud"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
@@ -33,6 +33,6 @@ func main() {
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "crud.com/unicorns",
+		Address: "crud.com/crud/unicorns",
 	})
 }
